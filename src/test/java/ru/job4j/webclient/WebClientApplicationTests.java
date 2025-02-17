083c530834908ab1fe50c900c347dd9928c21b3d
itlazykin <dzaka89@gmail.com>
2025-02-17 14:32:38 +0300

    init commit

---
+++ src/test/java/ru/job4j/webclient/WebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
