9ce7baa29c26b5b9bf2d3ce485b48a07f584a4c4
itlazykin <dzaka89@gmail.com>
2025-02-26 13:26:46 +0300

    add web client service

---
+++ services/webclient/src/main/java/ru/job4j/webclient/WebclientApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebclientApplication.class, args);
+	}
+
+}
